b16796b997130c272722b2cef953314c78631c18
hawk30 <gaurav1292@gmail.com>
2016-12-09 09:08:29 +0530

    messageBody writer

---
+++ src/main/java/DateMessageBodyWriter.java
@@ -0,0 +1,31 @@
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+import java.util.Date;
+
+/**
+ * Created by oracle on 9/12/16.
+ */
+@Provider
+public class DateMessageBodyWriter implements MessageBodyWriter<Date>{
+    @Override
+    public boolean isWriteable(Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return Date.class.isAssignableFrom(type);
+    }
+
+    @Override
+    public long getSize(Date date, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(Date date, Class<?> type, Type genericType, Annotation[] annotations, MediaType mediaType, MultivaluedMap<String, Object> httpHeaders, OutputStream entityStream) throws IOException, WebApplicationException {
+        entityStream.write(date.toString().getBytes());
+    }
+}
--- src/main/java/DateResource.java
+++ src/main/java/DateResource.java
@@ -3,6 +3,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by oracle on 7/12/16.
@@ -10,9 +12,19 @@ import javax.ws.rs.core.MediaType;
 @Path("date/{dateParam}")
 public class DateResource {
 
-    @GET
+    /*@GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getDate(@PathParam("dateParam") MyDate dateInfo){
         return "date is: "+dateInfo.toString();
+    }*/
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Date testMethod(){
+        return Calendar.getInstance().getTime();
     }
+
+
+
 }
+
